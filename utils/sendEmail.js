575f94a005c60d05aa00e3f382d774f76e314281
JAY_PARMAR <jayparmarit28@gmail.com>
2026-04-22 13:14:22 +0530

    Add files via upload

---
+++ utils/sendEmail.js
@@ -0,0 +1,21 @@
+import transporter from "../config/email.js";
+
+const sendEmail = async ({ to, subject, html}) => {
+
+            try {
+                
+                const info = await transporter.sendMail({
+                    from: 'quick nest "jayparmarj1@gmail.com"',
+                    to,
+                    subject,
+                    html
+                })
+
+                console.log("email sent id", info.messageId);
+
+            } catch (error) {
+                console.log(error.message)
+            }
+}
+
+export default sendEmail
